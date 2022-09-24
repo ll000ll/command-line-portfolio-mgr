db8701f028a942cc9440e0277663e4feea457671
michail <mail.mihail@protonmail.com>
2022-09-25 01:37:03 +0300

    add basic error handling in api fetcher

--- src/apiFetcher.ts
+++ src/apiFetcher.ts
@@ -6,6 +6,9 @@ import { defaultFetchPricesUrl } from "./defaultValues"
 export const getPriceFromApi = async (priceProviderUrl?: string) => {
   const url = priceProviderUrl || defaultFetchPricesUrl
   const response = await fetch(url)
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`)
+  }
   const data = await response.json()
   return data
 }
